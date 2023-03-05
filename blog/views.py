0ef0eb4a23e748ccb4292cb6a7cc715f3798365f
AngMaher <angelamaher@mail.com>
2023-03-05 14:10:00 +0000

    create view code and tranfer templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    #  list of posts status=1(approved) and ordered by when created
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    # name of html file that our view will be rendered to
+    template_name = 'index.html'
+    # paginate(separate into pages) limit 6 means will only show 6 posts
+    # on front page
+    paginate_by = 6
+    
