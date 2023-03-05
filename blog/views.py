bde38cc9eb200a4845c4195a643e3fe339bdb946
AngMaher <angelamaher@mail.com>
2023-03-05 14:47:53 +0000

    connect urls

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,7 @@ class PostList(generic.ListView):
     #  list of posts status=1(approved) and ordered by when created
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     # name of html file that our view will be rendered to
-    template_name = 'index.html'
+    template_name = "index.html"
     # paginate(separate into pages) limit 6 means will only show 6 posts
-    # on front page
+    # on front page, any more django will auto do page nav
     paginate_by = 6
-    
